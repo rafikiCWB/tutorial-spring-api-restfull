636413da8dd97b560638bac4411a06641a801906
rafinha <sta_rafinha@hotmail.com>
2024-06-26 12:08:24 -0300

    feat: projeto criado e application.properties configurado para uso do H2

---
+++ src/test/java/org/brisa/tutorialspring/TutorialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brisa.tutorialspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
