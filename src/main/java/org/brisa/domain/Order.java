6b6461bcb7c53588b6a65e9999dee91408d4af28
rafael <sta_rafinha@hotmail.com>
2024-08-08 15:42:43 -0300

    fix: Ajustado coluna status para aceitar tipo Enum.

--- src/main/java/org/brisa/domain/Order.java
+++ src/main/java/org/brisa/domain/Order.java
@@ -1,9 +1,6 @@
 package org.brisa.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
@@ -15,9 +12,13 @@ public class Order {
     @GeneratedValue
     private Long id;
     private String description;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
     private Status status;
 
-    public Order(){}
+    public Order() {
+    }
 
     public Order(String description, Status status) {
         this.description = description;
@@ -27,34 +28,42 @@ public class Order {
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public Status getStatus() {
         return status;
     }
+
     public void setStatus(Status status) {
         this.status = status;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
 
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Order order = (Order) o;
         return Objects.equals(id, order.id) && Objects.equals(description, order.description) && status == order.status;
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(id, description, status);
     }
+
     @Override
     public String toString() {
         return "Order{" +
